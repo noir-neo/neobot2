457f38bf89f6ace962daed80205fe94f8d757ef8
neo <noir.neo.04@gmail.com>
2015-12-04 15:51:36 +0900

    run 'cd handlers; lita handler chat'

---
+++ handlers/lita-chat/lib/lita-chat.rb
@@ -0,0 +1,12 @@
+require "lita"
+
+Lita.load_locales Dir[File.expand_path(
+  File.join("..", "..", "locales", "*.yml"), __FILE__
+)]
+
+require "lita/handlers/chat"
+
+Lita::Handlers::Chat.template_root File.expand_path(
+  File.join("..", "..", "templates"),
+ __FILE__
+)
---
+++ handlers/lita-chat/spec/lita/handlers/chat_spec.rb
@@ -0,0 +1,4 @@
+require "spec_helper"
+
+describe Lita::Handlers::Chat, lita_handler: true do
+end
